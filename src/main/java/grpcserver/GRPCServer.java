c61e3b8be96c6d5eadcb2631402126495b363567
harshal95 <psriharshal7@gmail.com>
2019-09-21 15:32:16 -0500

    added sample java code for server

--- src/main/java/grpcserver/GRPCServer.java
+++ src/main/java/grpcserver/GRPCServer.java
@@ -5,10 +5,19 @@ import io.grpc.ServerBuilder;
 import kvStore.KVStoreService;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class GRPCServer {
     public static void main(String []args) throws IOException, InterruptedException {
-        Server server = ServerBuilder.forPort(9090).addService(new KVStoreService()).build();
+//        String inputParams[] = args[0].split(" ");
+        int n = Integer.parseInt(args[0]);
+        int rank = Integer.parseInt(args[1]);
+        int updateNode = Integer.parseInt(args[2]);
+        int port = Integer.parseInt(args[3]);
+        System.out.println("length of array: " +  args.length);
+        System.out.println(Arrays.toString(args));
+
+        Server server = ServerBuilder.forPort(port).addService(new KVStoreService()).build();
         server.start();
 
         System.out.println("Server started at " + server.getPort());
